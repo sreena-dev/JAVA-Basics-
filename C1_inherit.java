d9dc6e84229a97d85a832b1d3eda50c1c9587ceb
Sreena M <138745389+sreena13@users.noreply.github.com>
2024-02-01 19:26:04 +0530

    Add files via upload

---
+++ C1_inherit.java
@@ -0,0 +1,10 @@
+public class C1_inherit 
+{
+    public static void main(String args[])
+    {
+        C1 val1= new C1();
+
+        System.out.println(val1.add(1,2) + " " + val1.sub(13,10));
+        System.out.println(val1.mult(2,3) + " " + val1.div(13,10));
+    }
+}
