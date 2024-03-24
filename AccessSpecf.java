ff5f92db2faf617a52f04d59ad443042efb3ebcd
Sreena M <138745389+sreena13@users.noreply.github.com>
2024-03-24 09:29:56 +0530

    Add files via upload

---
+++ AccessSpecf.java
@@ -0,0 +1,9 @@
+//from one package to other public
+//public scope=Anywhere
+//private scope=within the class
+//default scope=same package but not  
+//protacted scope= same class, same pkg subclass, sam pkg non subclass
+//diff pkg but not diff pkg non sub class
+public class AccessSpecf {
+    
+}
