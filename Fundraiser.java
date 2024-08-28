3c755d176fb5b6c4b0f07a67a1957adce53dca05
SREENA M <138745389+sreena13@users.noreply.github.com>
2024-08-28 14:46:10 +0530

    Add files via upload

---
+++ Fundraiser.java
@@ -0,0 +1,36 @@
+package basics;
+import java.math.*;
+import java.util.*;
+
+public class Fundraiser {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int n=3;
+		long a=0;
+		//BigInteger sum=BigInteger.ZERO;
+	//	BigInteger[] arr=new BigInteger[n];
+//		for(int i=0;i<n;i++) {
+//			arr[i]=sc.nextBigInteger();
+//		}
+		for(int i=0;i<n;i++) {
+			a=a+sc.nextLong();
+		}
+		System.out.println(a);
+		
+//		for(int i=0;i<n;i++) {
+//			sum=sum.add(arr[i]);
+//		}
+//		BigInteger aa=new BigInteger("343434343434343434343434343434");
+//		System.out.println(sum);
+//		System.out.println(aa);
+		
+		String armstrong="153";
+		int len=armstrong.length();
+		for(int i=0;i<len;i++) {
+			
+		}
+		sc.close();
+		
+	}
+	
+}
