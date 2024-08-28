56925a7f0595c265ab29df1f8433bd7571f57041
SREENA M <138745389+sreena13@users.noreply.github.com>
2024-08-28 14:46:18 +0530

    Add files via upload

---
+++ MultiContionTernary.java
@@ -0,0 +1,12 @@
+package basics;
+
+public class MultiContionTernary{
+	public static void main(String[] args){
+		int a=14;
+		int b=7;
+		int c=14;
+		
+		System.out.println(a>b? a>c:b>c? b:b>c? b:c);
+	}
+	
+}
